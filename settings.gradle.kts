d9bbc8fef5eb2d2568b83719d48816b54d8cda61
민수아 <Minsua0503@gmail.com>
2026-04-01 10:56:38 +0900

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "ZIPZA-BE"
---
+++ src/test/kotlin/com/example/zipzabe/ZipzaBeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.zipzabe
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ZipzaBeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
