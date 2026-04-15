e0aef3c5221f42868128a6d4101820ba04153d30
민수아 <Minsua0503@gmail.com>
2026-04-15 15:24:06 +0900

    feat: Swagger(SpringDoc OpenAPI) 세팅 추가

---
+++ src/main/kotlin/com/example/zipzabe/global/config/SwaggerConfig.kt
@@ -0,0 +1,18 @@
+package com.example.zipzabe.global.config
+
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class SwaggerConfig {
+    @Bean
+    fun openAPI(): OpenAPI = OpenAPI()
+        .info(
+            Info()
+                .title("ZIPZA API")
+                .description("ZIPZA 백엔드 API 문서")
+                .version("v1.0.0")
+        )
+}
